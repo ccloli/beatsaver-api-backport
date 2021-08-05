5f562e46066b41763fc1f7d26ca1bf9988dce3da
ccloli <h@ccloli.com>
2021-08-06 03:18:54 +0800

    feat: support text search (/api/search/text)

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,11 @@
 const Router = require('express').Router;
 const detail = require('../services/detail');
 const uploader = require('../services/uploader');
+const search = require('../services/search');
 
 const router = Router();
 router.use('/api/maps/detail/:id', detail);
 router.use('/api/maps/uploader/:id/:page', uploader);
+router.use('/api/search/text/:page', search);
 
 module.exports = router;
