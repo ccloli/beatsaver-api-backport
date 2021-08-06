df3dde5c91181dd81a95ae5a5eff551199f80313
ccloli <h@ccloli.com>
2021-08-06 20:54:50 +0800

    feat: support song stats (/api/stats/key)
    The response of song stat is a subset of song detail

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,13 @@ const detail = require('../services/detail');
 const uploader = require('../services/uploader');
 const search = require('../services/search');
 const download = require('../services/download');
+const stats = require('../services/stats');
 
 const router = Router();
 router.use('/api/maps/detail/:id', detail);
 router.use('/api/maps/uploader/:id/:page', uploader);
 router.use('/api/search/text/:page', search);
 router.use('/api/download/key/:id', download);
+router.use('/api/stats/key/:id', detail); 
 
 module.exports = router;
