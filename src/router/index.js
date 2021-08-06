eae706bd8ccad7fe2d88481f7452c5b76c63db6b
ccloli <h@ccloli.com>
2021-08-06 20:47:11 +0800

    feat: support map download (/api/download/key)

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,12 @@ const Router = require('express').Router;
 const detail = require('../services/detail');
 const uploader = require('../services/uploader');
 const search = require('../services/search');
+const download = require('../services/download');
 
 const router = Router();
 router.use('/api/maps/detail/:id', detail);
 router.use('/api/maps/uploader/:id/:page', uploader);
 router.use('/api/search/text/:page', search);
+router.use('/api/download/key/:id', download);
 
 module.exports = router;
---
+++ src/services/download.js
@@ -0,0 +1,13 @@
+const axios = require('axios');
+const { convertSong } = require('../utils/convert');
+
+const detail = async (req, res) => {
+	const { id } = req.params;
+
+	const response = await axios.get(`https://api.beatsaver.com/maps/id/${id}`);
+	const result = convertSong(response.data);
+
+	res.redirect(302, result.directDownload);
+};
+
+module.exports = detail;
