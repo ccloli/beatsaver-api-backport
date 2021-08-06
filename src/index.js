f716faa8cc55ebfc5dd5fbdcc350cee053d849a6
ccloli <h@ccloli.com>
2021-08-06 21:21:15 +0800

    feat: proxy browser requests to original server

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = require('./router');
+const proxy = require('express-http-proxy');
 const port = process.env.PORT || 9980;
 
 const app = express();
+app.use(proxy('https://beatsaver.com', {
+	filter: req => req.headers.referer || !req.path.startsWith('/api')
+}));
 app.use('/', router);
 app.listen(port, () => {
 	console.log(`Server is now listening on port ${port}`);
