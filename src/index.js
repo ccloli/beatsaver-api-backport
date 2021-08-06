1d3239dc9c91854a5958bef566c575e1a4c9d90e
ccloli <h@ccloli.com>
2021-08-06 21:26:43 +0800

    feat: proxies to api.beatsaver.com and set host header manually

--- src/index.js
+++ src/index.js
@@ -4,8 +4,15 @@ const proxy = require('express-http-proxy');
 const port = process.env.PORT || 9980;
 
 const app = express();
-app.use(proxy('https://beatsaver.com', {
-	filter: req => req.headers.referer || !req.path.startsWith('/api')
+app.use(proxy('https://api.beatsaver.com', {
+	// request api.beatsaver.com in case you point beatsaver.com to localhost,
+	// nslookup api.beatsaver.com says it's an alias of beatsaver.com,
+	// so it's safe to request api.beatsaver.com for now
+	proxyReqOptDecorator: (req) => {
+		req.headers.host = 'beatsaver.com';
+		return req;
+	},
+	filter: (req) => req.headers.referer || !req.path.startsWith('/api')
 }));
 app.use('/', router);
 app.listen(port, () => {
