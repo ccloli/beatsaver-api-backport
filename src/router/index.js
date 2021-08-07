7f2a3e307c14c88a3895a8f13638cb11fc26ab3a
ccloli <h@ccloli.com>
2021-08-07 14:35:30 +0800

    feat: support song stats by hash (/api/stats/by-hash)

--- src/router/index.js
+++ src/router/index.js
@@ -15,6 +15,7 @@ router.use('/api/maps/uploader/:id/:page', uploader);
 router.use('/api/search/text/:page', search);
 router.use('/api/download/key/:id', download);
 router.use('/api/stats/key/:id', detail);
+router.use('/api/stats/by-hash/:hash', detail);
 router.use('/api/maps/latest/:page', latest);
 router.use('/api/maps/rating/:page', rating);
 router.use('/api/maps/plays/:page', plays);
